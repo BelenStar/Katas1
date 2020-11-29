5146988046ebf20fef18c7cc83477cca5b06eb13
Nelly <estrelladebelen2000@gmail.com>
2020-11-29 16:30:02 -0400

    codigo del katas hecho en produccion

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,85 @@
+import java.util.Scanner
+
+class Roman {
+    fun convert(s: String): Int {
+        val letter: CharArray = s.toCharArray()
+        var arab = 0
+
+        for(i in 0..letter.lastIndex) {
+            if (letter[i] == 'I') {
+                arab += 1;
+                continue;
+            }
+
+            if (letter[i] == 'V') {
+                arab += 5;
+                if(i == 0)
+                    continue;
+                if(letter[i-1] == 'I'){
+                    arab -= 2
+                    continue;
+                }
+            }
+            if (letter[i] == 'X') {
+                arab += 10;
+                if(i == 0)
+                    continue;
+                if(letter[i-1] == 'I'){
+                    arab -= 2
+                    continue;
+                }
+            }
+            if (letter[i] == 'L') {
+                arab += 50;
+                if(i == 0)
+                    continue;
+                if(letter[i-1] == 'X'){
+                    arab -= 20
+                    continue;
+                }
+            }
+
+            if (letter[i] == 'C') {
+                arab += 100;
+                if(i == 0)
+                    continue;
+                if(letter[i-1] == 'X'){
+                    arab -= 20
+                    continue;
+                }
+            }
+
+            if (letter[i] == 'D') {
+                arab += 500;
+                if(i == 0)
+                    continue;
+                if(letter[i-1] == 'C'){
+                    arab -= 200
+                    continue;
+                }
+            }
+
+            if (letter[i] == 'M') {
+                arab += 1000;
+                if(i == 0)
+                    continue;
+                if(letter[i-1] == 'C'){
+                    arab -= 200
+                    continue;
+                }
+            }
+        }
+
+        return  arab
+    }
+
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val input = Scanner(System.`in`)
+    println("Escribe en mayuscula el numero romano que quieres convertir:")
+    val roman = input.next()
+    val arab = Roman().convert(roman)
+    println("El numero $roman convertido en arabigo es: $arab")
+
 }
